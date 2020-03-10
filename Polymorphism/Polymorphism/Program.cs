b025b91b06f85f8a96093bffbc125942c52e80af
Richard Mutembereza <Richard.Mutembereza@derivco.co.za>
2020-03-10 15:57:42 +0200

    Add Fail tests

--- Polymorphism/Polymorphism/Program.cs
+++ Polymorphism/Polymorphism/Program.cs
@@ -5,23 +5,30 @@ namespace Polymorphism
 {
     public class Employee
     {
-        public virtual void CalculateWeeklySalary(int weeklyHours, int wage)
+        public virtual string CalculateWeeklySalary(int weeklyHours, int wage)
         {
             var salary = 40 * wage;
-            Console.WriteLine("\nThis ANGRY EMPLOYEE worked {0} hrs. " + 
-                              "Paid for 40 hrs at $ {1}" +
-                              "/hr = ${2} \n", weeklyHours, wage, salary);
+            string result = String.Format("This ANGRY EMPLOYEE worked {0} hrs. " + 
+                                          "Paid for 40 hrs at $ {1}" +
+                                          "/hr = ${2} \n", weeklyHours, wage, salary);
+
+            Console.WriteLine("\n" + result + "\n");
             Console.WriteLine("---------------------------------------------\n");
+
+            return result;
         }
     }
 
     public class Contractor : Employee
     {
-        public override void CalculateWeeklySalary(int weeklyHours, int wage)
+        public override string CalculateWeeklySalary(int weeklyHours, int wage)
         {
             var salary = weeklyHours * wage;
-            Console.WriteLine($"\nThis HAPPY CONTRACTOR worked {weeklyHours} hrs. Paid for {weeklyHours} hrs at $ {wage} /hr = ${salary} ");
+            string result = String.Format("This HAPPY CONTRACTOR worked {weeklyHours} hrs.Paid for {weeklyHours} hrs at $ {wage} / hr = ${salary}" );
+            
+            Console.WriteLine($"\n " + result);
             Console.WriteLine("---------------------------------------------\n");
+            return result;
         }
     }
 
--- Polymorphism/PolymorphismTestProject/UnitTest1.cs
+++ Polymorphism/PolymorphismTestProject/UnitTest1.cs
@@ -1,14 +1,79 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Polymorphism;
 
 namespace PolymorphismTestProject
 {
-    [TestClass]
-    public class UnitTest1
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void CalculateWeeklySalaryForEmployeeTest_70wage55hoursReturns2800Dollars()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+      //Arrange
+      int weeklyHours = 55;
+      int wage = 70;
+      int salary = 1;
+      
+      Employee e = new Employee();
+      string expectedR = String.Format("This ANGRY EMPLOYEE worked {0} hrs. " +
+                                       "Paid for 40 hrs at $ {1}" +
+                                       "/hr = ${2} \n", weeklyHours, wage, salary);
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+      //Assert
+      Assert.AreEqual(response, expectedR);
+    }
+    
+    [TestMethod]
+    public void CalculateWeeklySalaryForContractore55hoursReturns3850Dollars()
+    {
+      int weeklyHours = 55;
+      int wage = 70;
+      int salary = 1;
+
+      Contractor e = new Contractor();
+
+      string expRes = $"This HAPPY CONTRACTOR worked {weeklyHours} hrs.Paid for {weeklyHours} hrs at {wage} / hr = {salary}";
+
+      string response = e.CalculateWeeklySalary(weeklyHours, wage);
+      
+      Assert.AreEqual(response,expRes);
+
+    }
+
+    [TestMethod]
+    public void CalculateWeeklySalaryForEmployeeTest_70wage55hoursDoesNotReturnCorrectString()
+    {
+      //Arrange
+      int weeklyHours = 55;
+      int wage = 70;
+      int salary = 40 * wage;
+
+      Employee e = new Employee();
+      string expectedR = String.Format("This ANGRY EMPLOYEE worked {0} hrs. " +
+                                       "Paid for 40 hrs at $ {1}" +
+                                       "/hr = ${2} \n", weeklyHours, wage, salary);
+            //Act
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+      //Assert
+      Assert.AreNotEqual(response, expectedR);
+    }
+
+    [TestMethod]
+    public void CalculateWeeklySalaryForContractorTest_70wag55hoursDoesNotReturnCorrectStrings()
+    {
+      int weeklyHours = 55;
+      int wage = 70;
+      int salary = weeklyHours * wage;
+
+      Contractor e = new Contractor();
+
+      string expRes = $"This HAPPY CONTRACTOR worked {weeklyHours} hrs.Paid for {weeklyHours} hrs at {wage} / hr = {salary}";
+
+      string response = e.CalculateWeeklySalary(weeklyHours, wage);
+
+      Assert.AreNotEqual(response,expRes);
+
+    }
     }
 }
